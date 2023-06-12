f11df95724d18f4fc4cf7e0b23a8bfd4baa042cf
Emilio Vesprini <emivespa1@gmail.com>
2023-06-12 19:16:43 +0000

    remove system colors from tailwind.config.js
    You don't need that to use them.

--- app/prose.tsx
+++ app/prose.tsx
@@ -8,15 +8,16 @@ export default function Prose({ children }: { children: React.ReactNode }) {
       max-w-[50rem]
       p-2
       dark:prose-invert
-      prose-p:text-canvastext
+      prose-h1:text-[Highlight]
+      prose-p:text-[canvastext]
+      prose-code:bg-[buttonface]
       prose-code:font-normal
-      prose-code:text-canvastext
+      prose-code:text-[canvastext]
       prose-code:before:content-['']
       prose-code:after:content-['']
-      prose-code:bg-buttonface
-      prose-pre:bg-buttonface
-      prose-pre:text-canvastext
-      prose-li:text-canvastext
+      prose-pre:bg-[buttonface]
+      prose-pre:text-[canvastext]
+      prose-li:text-[canvastext]
       prose-img:mx-auto
       prose-img:max-h-[20rem]
       "
