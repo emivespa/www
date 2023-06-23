decc3ed244c67e044f5ce9404ef495756810a021
Emilio Vesprini <emivespa1@gmail.com>
2023-06-23 12:55:15 +0000

    update nix.mdx

--- app/[mdx]/page.tsx
+++ app/[mdx]/page.tsx
@@ -1,6 +1,8 @@
-import dynamic from 'next/dynamic'
+import dynamic from "next/dynamic"
 
 export default function Page({ params }: any) {
-  const Mdx = dynamic(() => import(`./${params.mdx ? params.mdx : 'index'}.mdx`))
+  const Mdx = dynamic(
+    () => import(`./${params.mdx ? params.mdx : "index"}.mdx`)
+  )
   return <Mdx />
 }
